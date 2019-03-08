cac64a7ce599930ec8260ab885ccd5d966086330
InfiniteGreen <46692630+InfiniteGreen@users.noreply.github.com>
2019-03-08 17:00:59 -0500

    updated main part1 & part2 .py file

--- main_part1.py
+++ main_part1.py
@@ -16,59 +16,87 @@ import csv
 budget_path = os.path.join('PyBank','Resources','budget_data.csv')
 
 
-# In[18]:
+# In[3]:
 
 
-#read file
+#define variables
 total=0
-totalfirstrow=0
-ave_change=0
+new_buget_list=[]
 max_profit_date=''
 max_profit_num=0
 min_profit_date=''
 min_profit_num=0
 
+
+# In[4]:
+
+
+#read file
+
 with open (budget_path, newline='') as budget_csv:
     #split into columns
     budget_reader = csv.reader(budget_csv,delimiter=',')
-    #header = next(budget_reader)
+    #print(budget_reader)
+    header = next(budget_reader)
     #Loop through the data
-    for row in budget_reader:
-        #print (row)
-        totalfirstrow+=1
-        if row[1].isnumeric():
-            #total += int(row[1])
-            if int(row[1])>=0:
-                if int(row[1])>max_profit_num:
-                    max_profit_num = int(row[1])
-                    max_profit_date = row[0]
-                    total+= int(row[1])
-            else:
-                if int(row[1])> min_profit_num:
-                    min_profit_num = int(row[1])
-                    min_profit_date = row[0]
-                    total-= int(row[1])
-                   
-    totalfirstrow-=1
-    print(str(total))
-  
+    for row in budget_reader:              
+        total += int(row[1])
+        new_buget_list.append(int(row[1]))
+        if int(row[1])>=0:
+            if int(row[1])>max_profit_num:
+                max_profit_num = int(row[1])
+                max_profit_date = row[0]
+        else:
+            if int(row[1])<min_profit_num:
+                min_profit_num = int(row[1])
+                min_profit_date = row[0]
+
+
+# In[5]:
+
+
+#print(new_buget_list)
+#print(len(new_buget_list))
+total_month = len(new_buget_list)
+#Calculate Average  Change
+ave_change = round((new_buget_list[total_month-1]-new_buget_list[0])/total_month,2)    
+
+
+# In[6]:
+
+
+#Print results
+print("Financial Analysis")
+print("==================")
+print('Total Months: ' + str(total_month))
+print('Total: $' + str(total))
+print('Average  Change: $' + str(ave_change))
+print('Greatest Increase in Profits: ' + max_profit_date + ' ($' + str(max_profit_num) + ')')
+print('Greatest Decrease in Profits: ' + min_profit_date + ' ($' + str(min_profit_num) + ')')
+
+
+# In[7]:
+
+
+# Specify the file to write to
+output_path = os.path.join("budget_out.csv")
 
 
 # In[8]:
 
 
-#Print result
-print("Financial Analysis")
-print("==================")
-print('Total Months: ' + str(totalfirstrow))
-print('Total: ' + str(total))
-print('Average  Change: $' + str(total/totalfirstrow))
-print('Greatest Increase in Profits: ' + max_profit_date + ' ($' + str(max_profit_num) + ')')
-print('Greatest Decrease in Profits: ' + min_profit_date + ' ($' + str(min_profit_num) + ')')
+# Open the file using "write" mode
+with open(output_path, 'w', newline='') as budgetfile:
 
+    # Initialize csv.writer
+    csvwriter = csv.writer(budgetfile)
 
-# In[ ]:
-
-
-
+    # Write the rows
+    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow(["=================="])
+    csvwriter.writerow(["Total Months: " + str(total_month)])
+    csvwriter.writerow(['Total: $' + str(total)])
+    csvwriter.writerow(['Average  Change: $' + str(ave_change)])
+    csvwriter.writerow(['Greatest Increase in Profits: ' + max_profit_date + ' ($' + str(max_profit_num) + ')'])
+    csvwriter.writerow(['Greatest Decrease in Profits: ' + min_profit_date + ' ($' + str(min_profit_num) + ')'])
 
---
+++ main_part2.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Dependencies
+import os
+import csv
+
+
+# In[2]:
+
+
+#filepath budget_data.csv
+poll_path = os.path.join('PyPoll','Resources','election_data.csv')
+
+
+# In[3]:
+
+
+#define variables
+candidate_list=[]
+unique_candidate_list=[]
+
+
+# In[4]:
+
+
+#read file
+
+with open (poll_path, newline='') as poll_csv:
+    #split into columns
+    poll_reader = csv.reader(poll_csv,delimiter=',')    
+    header = next(poll_reader)
+    #Loop through the data
+    for row in poll_reader:            
+        candidate_list.append(row[2])
+        if row[2] not in unique_candidate_list:
+            unique_candidate_list.append(row[2])
+#unique_candidate_list
+
+
+# In[5]:
+
+
+# Get total votes
+totalvotes = len(candidate_list)
+
+
+# In[6]:
+
+
+k=0
+vote_count=[]
+vote_percent=[]
+while k<len(unique_candidate_list):
+    vote_count.append(candidate_list.count(str(unique_candidate_list[k])))    
+    vote_percent.append("{:.3%}".format(vote_count[k]/totalvotes))
+
+#vote_count   
+#vote_percent 
+#winner    
+
+
+# In[19]:
+
+
+#Get the Poll winner
+winner=unique_candidate_list[vote_count.index(max(vote_count))]
+winner
+
+
+# In[7]:
+
+
+#Print results
+print("Election Results")
+print("---------------------------")
+print('Total Votes: ' + str(totalvotes))
+print("---------------------------")   
+
+j=0
+while j<len(unique_candidate_list):
+    print(str(unique_candidate_list[j]) + ': ' + str(vote_percent[j]) + ' (' + str(vote_count[j]) + ')')    
+    j+=1
+    
+print("---------------------------") 
+print('Winner: ' + str(winner))
+print("---------------------------") 
+
+
+# In[10]:
+
+
+# Specify the file to write to
+output_path = os.path.join("poll_out.csv")
+
+
+# In[12]:
+
+
+# Open the file using "write" mode
+with open(output_path, 'w', newline='') as pollfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(pollfile)
+
+    # Write the rows
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(["---------------------------"])
+    csvwriter.writerow(['Total Votes: ' + str(totalvotes)])
+    csvwriter.writerow(["---------------------------"])
+    
+    i=0
+    while i<len(unique_candidate_list):
+        csvwriter.writerow([str(unique_candidate_list[i]) + ': ' + str(vote_percent[i]) + ' (' + str(vote_count[i]) + ')'])
+        i+=1
+    
+    csvwriter.writerow(["---------------------------"]) 
+    csvwriter.writerow(['Winner: ' + str(winner)])
+    csvwriter.writerow(["---------------------------"]) 
+
